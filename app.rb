95cbda662ca9ed56a869d4d00dc943793bcbc4a7
Cheryl K <cherylkelson@icloud.com>
2019-03-09 20:11:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,9 +8,13 @@ class App < Sinatra::Base
     erb :index
   end
 
+
   post '/' do
-    @analyzed_text = TextAnalyzer.new(params[:user_text])
-    erb :results
-  end
+  text_from_user = params[:user_text]
+
+  @analyzed_text = TextAnalyzer.new(text_from_user)
+
+  erb :results
+end
 
 end
